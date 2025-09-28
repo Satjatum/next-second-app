67980d6a494270b5527ae172d10f34c86840ddb1
kazaza021 <pooklnw.2546@gmail.com>
2025-09-28 17:16:23 +0700

    แก้ไข student page

--- app/student/[id]/page.tsx
+++ app/student/[id]/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { promises } from "dns";
+import React from "react";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <div>
       <h1>Student ID : {id}</h1>
       <hr />
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, illo.</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+        illo.
+      </p>
     </div>
-  )
+  );
 }
